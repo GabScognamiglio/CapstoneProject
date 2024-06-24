c32ede2c60dba941f0895372fb56b85862589301
GabScognamiglio <gabbo16s@gmail.com>
2024-06-24 16:47:45 +0200

    updated methods for balances

--- src/main/java/it/epicode/gs_budgets/service/AccountService.java
+++ src/main/java/it/epicode/gs_budgets/service/AccountService.java
@@ -89,7 +89,7 @@ public class AccountService {
 
     public BalanceEntry getAccountBalanceLastMonth(int accountId) {
         Account account = getAccountById(accountId);
-        return calculateBalanceLastMonth(account);
+        return calculateBalanceCurrentMonth(account);
     }
 
     public BalanceEntry getAccountBalanceLastWeek(int accountId) {
@@ -99,41 +99,92 @@ public class AccountService {
 
 
     //BILANCIO + SPESE + ENTRATE MESE PER MESE ULTIMI 12
+//    public Map<String, BalanceEntry> getAccountBalancesMonthlyLast12Months(int accountId) {
+//        Account account = getAccountById(accountId);
+//        Map<String, BalanceEntry> balances = new LinkedHashMap<>();
+//
+//        LocalDate today = LocalDate.now();
+//        LocalDate startDate = today.minusMonths(12).withDayOfMonth(1);
+//        LocalDate endDate = today.withDayOfMonth(1).minusDays(1);
+//
+//        LocalDate tempDate = startDate;
+//        while (!tempDate.isAfter(endDate)) {
+//            LocalDate finalTempDate = tempDate;
+//
+//            double totalIncome = account.getIncomes().stream()
+//                    .filter(income -> income.getDate().getMonth() == finalTempDate.getMonth() && income.getDate().getYear() == finalTempDate.getYear())
+//                    .mapToDouble(Income::getAmount)
+//                    .sum();
+//
+//            double totalExpense = account.getExpenses().stream()
+//                    .filter(expense -> expense.getDate().getMonth() == finalTempDate.getMonth() && expense.getDate().getYear() == finalTempDate.getYear())
+//                    .mapToDouble(Expense::getAmount)
+//                    .sum();
+//
+//            double balance = totalIncome - totalExpense;
+//
+//            BalanceEntry entry = new BalanceEntry();
+//            entry.setBalance(balance);
+//            entry.setTotalIncome(totalIncome);
+//            entry.setTotalExpense(totalExpense);
+//
+//            balances.put(finalTempDate.getMonth().toString() + " " + finalTempDate.getYear(), entry);
+//            tempDate = tempDate.plusMonths(1);
+//        }
+//
+//        return balances;
+//    }
+
     public Map<String, BalanceEntry> getAccountBalancesMonthlyLast12Months(int accountId) {
         Account account = getAccountById(accountId);
         Map<String, BalanceEntry> balances = new LinkedHashMap<>();
 
         LocalDate today = LocalDate.now();
+        // Calcola la data di inizio 12 mesi fa da oggi
         LocalDate startDate = today.minusMonths(12).withDayOfMonth(1);
+        // Calcola la data di fine come il mese precedente rispetto ad oggi
         LocalDate endDate = today.withDayOfMonth(1).minusDays(1);
 
-        LocalDate tempDate = startDate;
+        // Parti da luglio dell'anno precedente rispetto all'anno corrente
+        LocalDate tempDate = startDate.plusMonths(1).withMonth(7);
+
         while (!tempDate.isAfter(endDate)) {
-            LocalDate finalTempDate = tempDate;
+            calculateBalanceForDate(account, tempDate, balances);
 
-            double totalIncome = account.getIncomes().stream()
-                    .filter(income -> income.getDate().getMonth() == finalTempDate.getMonth() && income.getDate().getYear() == finalTempDate.getYear())
-                    .mapToDouble(Income::getAmount)
-                    .sum();
-
-            double totalExpense = account.getExpenses().stream()
-                    .filter(expense -> expense.getDate().getMonth() == finalTempDate.getMonth() && expense.getDate().getYear() == finalTempDate.getYear())
-                    .mapToDouble(Expense::getAmount)
-                    .sum();
-
-            double balance = totalIncome - totalExpense;
-
-            BalanceEntry entry = new BalanceEntry();
-            entry.setBalance(balance);
-            entry.setTotalIncome(totalIncome);
-            entry.setTotalExpense(totalExpense);
-
-            balances.put(finalTempDate.getMonth().toString() + " " + finalTempDate.getYear(), entry);
+            // Avanza al mese successivo
             tempDate = tempDate.plusMonths(1);
+        }
+
+        // Calcola il bilancio anche per il mese corrente (giugno 2024)
+        if (today.getMonthValue() == 6) { // Se siamo a giugno
+            calculateBalanceForDate(account, today, balances);
         }
 
         return balances;
     }
+
+    private void calculateBalanceForDate(Account account, LocalDate date, Map<String, BalanceEntry> balances) {
+        double totalIncome = account.getIncomes().stream()
+                .filter(income -> income.getDate().getMonth() == date.getMonth() && income.getDate().getYear() == date.getYear())
+                .mapToDouble(Income::getAmount)
+                .sum();
+
+        double totalExpense = account.getExpenses().stream()
+                .filter(expense -> expense.getDate().getMonth() == date.getMonth() && expense.getDate().getYear() == date.getYear())
+                .mapToDouble(Expense::getAmount)
+                .sum();
+
+        double balance = totalIncome - totalExpense;
+
+        BalanceEntry entry = new BalanceEntry();
+        entry.setBalance(balance);
+        entry.setTotalIncome(totalIncome);
+        entry.setTotalExpense(totalExpense);
+
+        balances.put(date.getMonth().toString() + " " + date.getYear(), entry);
+    }
+
+
 
     //BILANCIO + SPESE + ENTRATE SETTIMANA PER SETTIMANA ULTIME 4
     public Map<String, BalanceEntry> getAccountBalancesWeeklyLast4Weeks(int accountId) {
@@ -206,13 +257,16 @@ public class AccountService {
         LocalDate today = LocalDate.now();
         LocalDate monthsAgo = today.minusMonths(months);
 
+        // Per evitare operazioni future, definiamo una data massima pari a oggi.
+        LocalDate maxDate = today;
+
         double totalIncome = account.getIncomes().stream()
-                .filter(income -> income.getDate().isAfter(monthsAgo))
+                .filter(income -> !income.getDate().isAfter(maxDate) && income.getDate().isAfter(monthsAgo))
                 .mapToDouble(Income::getAmount)
                 .sum();
 
         double totalExpense = account.getExpenses().stream()
-                .filter(expense -> expense.getDate().isAfter(monthsAgo))
+                .filter(expense -> !expense.getDate().isAfter(maxDate) && expense.getDate().isAfter(monthsAgo))
                 .mapToDouble(Expense::getAmount)
                 .sum();
 
@@ -225,20 +279,21 @@ public class AccountService {
 
         return entry;
     }
+
 
     //BILANCIO ULTIMO MESE
-    private BalanceEntry calculateBalanceLastMonth(Account account) {
+    private BalanceEntry calculateBalanceCurrentMonth(Account account) {
         LocalDate today = LocalDate.now();
-        LocalDate lastMonthStart = today.minusMonths(1).withDayOfMonth(1);
-        LocalDate lastMonthEnd = today.minusMonths(1).withDayOfMonth(today.minusMonths(1).lengthOfMonth());
+        LocalDate currentMonthStart = today.withDayOfMonth(1); // Primo giorno del mese corrente
+        LocalDate currentMonthEnd = today.withDayOfMonth(today.lengthOfMonth()); // Ultimo giorno del mese corrente
 
         double totalIncome = account.getIncomes().stream()
-                .filter(income -> income.getDate().isAfter(lastMonthStart) && income.getDate().isBefore(lastMonthEnd.plusDays(1)))
+                .filter(income -> income.getDate().isAfter(currentMonthStart.minusDays(1)) && income.getDate().isBefore(currentMonthEnd.plusDays(1)))
                 .mapToDouble(Income::getAmount)
                 .sum();
 
         double totalExpense = account.getExpenses().stream()
-                .filter(expense -> expense.getDate().isAfter(lastMonthStart) && expense.getDate().isBefore(lastMonthEnd.plusDays(1)))
+                .filter(expense -> expense.getDate().isAfter(currentMonthStart.minusDays(1)) && expense.getDate().isBefore(currentMonthEnd.plusDays(1)))
                 .mapToDouble(Expense::getAmount)
                 .sum();
 
@@ -251,6 +306,7 @@ public class AccountService {
 
         return entry;
     }
+
 
     //BILANCIO ULTIMA SETTIMANA
     private BalanceEntry calculateBalanceLastWeek(Account account) {
